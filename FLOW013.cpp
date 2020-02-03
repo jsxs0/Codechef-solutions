6f59eaebedf72891d19c3cbaefa4dcd82881d1ac
Jasveen <44266373+jsxs0@users.noreply.github.com>
2020-02-03 15:22:29 +0530

    Done!
    3 
    40 40 100
    45 45 90
    180 1 1

---
+++ FLOW013.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int A,B,C;
+	    cin>>A;
+	    cin>>B;
+	    cin>>C;
+	    if(A+B+C==180)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
